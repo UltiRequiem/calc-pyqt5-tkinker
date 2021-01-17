0bc62fb5e59b6f28c8cb19197c8d408499b802de
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-17 08:02:00 -0500

    More Buttons

--- main.py
+++ main.py
@@ -30,7 +30,19 @@ def button_equal():
     e.insert(0, f_num + int(second_number))
 
 
-# Define Buttons
+def button_divide(args):
+    pass
+
+
+def button_multiply(args):
+    pass
+
+
+def button_subtract(args):
+    pass
+
+
+# Number Buttons
 
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
 button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
@@ -42,10 +54,15 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-button_add = Button(root, text="+", padx=40, pady=20, command=button_add)
 
+# Signs Buttons
+
+button_add = Button(root, text="+", padx=40, pady=20, command=button_add)
 button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
 button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+button_subtract = Button(root, text="-", padx=40, pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=91, pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=79, pady=20, command=button_divide)
 
 # Put The buttons on the screen
 
