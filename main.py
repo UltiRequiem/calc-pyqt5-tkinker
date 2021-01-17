ab1af4b52e241548390bd6e2d0008cb22f245c34
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-17 07:50:02 -0500

    Working

--- main.py
+++ main.py
@@ -20,8 +20,14 @@ def button_clear():
 def button_add():
     first_number = e.get()
     global f_num
-    f_num: int = int(first_number)
+    f_num = int(first_number)
     e.delete(0, END)
+
+
+def button_equal():
+    second_number = e.get()
+    e.delete(0, END)
+    e.insert(0, f_num + int(second_number))
 
 
 # Define Buttons
@@ -37,7 +43,8 @@ button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
 button_add = Button(root, text="+", padx=40, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=91, pady=20, command=lambda: button_click())
+
+button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
 button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
 
 # Put The buttons on the screen
